cc3f709b2a05d2ae3001bd13860540b7a687717b
Namibo06 <namibo654@gmail.com>
2024-09-15 13:52:28 -0300

    chhre: rename file

--- src/js/chancesRemaining.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const chancesRemaining = document.getElementById('chances');
-    let chances = 6;
-    chancesRemaining.textContent=chances;
-});
