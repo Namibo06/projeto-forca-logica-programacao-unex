738f2522b9c6f229d693a8f2c1a7599340669db0
Namibo06 <namibo654@gmail.com>
2024-09-15 12:15:39 -0300

    feat: chances

---
+++ src/js/chancesRemaining.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const chancesRemaining = document.getElementById('chances');
+    let chances = 6;
+    chancesRemaining.textContent=chances;
+});
